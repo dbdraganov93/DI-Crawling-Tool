bc75998995a035e4481b10d13417845293b19400
Dragan Draganov <dragan.draganov@offerista.com>
2025-06-08 14:23:59 +0300

    Working autolocale

---
+++ src/Service/LocaleService.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Service;
+
+class LocaleService
+{
+    const LOCALE = [
+       231  => 'it_it',
+       239  => 'es_es',
+       241  => 'fr_fr',
+       242  => 'pt_pt',
+       244  => 'en_au',
+    ];
+
+    public function getLocale($ownerId): string
+    {
+        return self::LOCALE[$ownerId] ?? '';
+    }
+}
