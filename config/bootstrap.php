d83592937d0c8b93b8fa38caad335c770762dd58
Dragan Draganov <dragan.draganov@offerista.com>
2025-06-18 14:30:58 +0300

    Env exclude from bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 use Symfony\Component\Dotenv\Dotenv;
 
-$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
+$appEnv = getenv('APP_ENV') ?: ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null);
 
-if ($env !== 'prod' && is_readable(dirname(__DIR__).'/.env')) {
+if (!$appEnv && is_readable(dirname(__DIR__).'/.env')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
