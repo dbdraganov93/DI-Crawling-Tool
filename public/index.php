fdb355d7200d8a852cadeaba71a069cf8c049e12
Strahil Hristov <strahil.hristov@offerista.com>
2025-06-18 13:34:37 +0300

    do not load .env file in prod

--- public/index.php
+++ public/index.php
@@ -1,6 +1,15 @@
 <?php
 use App\Kernel;
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    if (($context['APP_ENV'] ?? 'prod') === 'prod') {
+        $context['dotenv_path'] = false;
+    }
+
+    return new Kernel(
+        $context['APP_ENV'] ?? 'prod',
+        (bool) ($context['APP_DEBUG'] ?? false)
+    );
 };
