165587e4610e29e2ca0a279e7ebe58171b49b4cf
Strahil Hristov <strahil.hristov@offerista.com>
2025-06-18 13:57:43 +0300

    rever index_php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,9 @@
 <?php
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    if (($context['APP_ENV'] ?? 'prod') === 'prod') {
-        $context['dotenv_path'] = false;
-    }
-
-    return new Kernel(
-        $context['APP_ENV'] ?? 'prod',
-        (bool) ($context['APP_DEBUG'] ?? false)
-    );
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
