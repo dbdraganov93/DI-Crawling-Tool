b7e260a5a0d0f452519f94c25f323ff68655b78c
Strahil Hristov <strahil.hristov@offerista.com>
2025-06-18 13:32:20 +0300

    adding missing env variable

--- public/index.php
+++ public/index.php
@@ -1,16 +1,6 @@
 <?php
-
 use App\Kernel;
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
 return function (array $context) {
-    if (($context['APP_ENV'] ?? 'prod') === 'prod') {
-        $context['dotenv_path'] = false; // Disable .env loading in prod
-    }
-
-    return new Kernel(
-        $context['APP_ENV'] ?? 'prod',
-        (bool) ($context['APP_DEBUG'] ?? false)
-    );
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
