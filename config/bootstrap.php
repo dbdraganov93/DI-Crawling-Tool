403a7ab4ad3b725164309546a1e1c5b116e6dbab
Strahil Hristov <strahil.hristov@offerista.com>
2025-06-19 05:57:59 +0300

    passing dummy .env values

---
+++ config/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+use Symfony\Component\Dotenv\Dotenv;
+
+if (!getenv('APP_ENV') && !isset($_ENV['APP_ENV']) && !isset($_SERVER['APP_ENV'])) {
+    $envFile = dirname(__DIR__) . '/.env';
+    if (is_readable($envFile)) {
+        (new Dotenv())->bootEnv($envFile);
+    }
+}
