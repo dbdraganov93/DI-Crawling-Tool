860c0de221658b2aa864480c36f216392a7c977a
viktor.yordanov <viktor.yordanov@offerista.com>
2025-07-22 17:42:25 +0300

    removing debug code

--- src/CrawlerScripts/ShopfullyCrawler.php
+++ src/CrawlerScripts/ShopfullyCrawler.php
@@ -55,7 +55,6 @@ class ShopfullyCrawler
             $stores = array_merge($stores, $this->createStores($sfBrochure['brochureStores']));
 
             $brochureData = $this->prepareBrochureData($sfBrochure['brochureData'], $brochureDetail);
-            dd($brochureData);
             $brochures[] = $this->createBrochure($brochureData);
 
             if (!empty($requestData['prefix']) || !empty($requestData['suffix'])) {
